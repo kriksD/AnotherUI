2ce222e45a79ff3443def50ba4af413a8e179c4f
kriksD <82215311+kriksD@users.noreply.github.com>
2024-01-23 11:42:03 +0200

    Fixed a bug with an unloaded image for newly created characters

--- src/main/kotlin/character/ACharacter.kt
+++ src/main/kotlin/character/ACharacter.kt
@@ -5,14 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toAwtImage
-import androidx.compose.ui.graphics.toComposeImageBitmap
 import getImageBitmap
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.jetbrains.skia.EncodedImageFormat
 import org.jetbrains.skiko.toImage
-import java.awt.image.BufferedImage
 import java.io.File
 import java.util.*
 
@@ -22,69 +20,7 @@ class ACharacter(
     val metaData: CharacterMetaData,
     image: ImageBitmap? = null,
 ) {
-    /*var image: ImageBitmap? = run {
-        /*val newSize = if (image.width / image.height > 400 / 600) {
-            calculateScaledDownSize(image.width, image.height, Int.MAX_VALUE, style.image_height * 2)
-        } else {
-            calculateScaledDownSize(image.width, image.height, style.image_width * 2, Int.MAX_VALUE)
-        }
-
-        image.scaleAndCropImage(newSize.first, newSize.second)*/
-        image
-    }
-        set(value) {
-            /*val newSize = if (image.width / image.height > 400 / 600) {
-                calculateScaledDownSize(image.width, image.height, Int.MAX_VALUE, style.image_height * 2)
-            } else {
-                calculateScaledDownSize(image.width, image.height, style.image_width * 2, Int.MAX_VALUE)
-            }
-
-            field = value.scaleAndCropImage(newSize.first, newSize.second)*/
-            field = value
-        }*/
-
     var image: ImageBitmap? by mutableStateOf(image)
-
-    private fun ImageBitmap.scaleAndCropImage(width: Int, height: Int): ImageBitmap {
-        val bufferedImage = this.toAwtImage()
-
-        val outputImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-
-        val scaleX = width.toDouble() / bufferedImage.width.toDouble()
-        val scaleY = height.toDouble() / bufferedImage.height.toDouble()
-        val scale = kotlin.math.max(scaleX, scaleY)
-
-        val scaledWidth = (bufferedImage.width * scale).toInt()
-        val scaledHeight = (bufferedImage.height * scale).toInt()
-
-        val scaledImage = BufferedImage(scaledWidth, scaledHeight, BufferedImage.TYPE_INT_RGB)
-        val g2d = scaledImage.createGraphics()
-        g2d.drawImage(bufferedImage, 0, 0, scaledWidth, scaledHeight, null)
-        g2d.dispose()
-
-        val x = (scaledWidth - width) / 2
-        val y = (scaledHeight - height) / 2
-
-        val croppedImage = scaledImage.getSubimage(x, y, width, height)
-
-        val g2dOut = outputImage.createGraphics()
-        g2dOut.drawImage(croppedImage, 0, 0, null)
-        g2dOut.dispose()
-
-        return outputImage.toComposeImageBitmap()
-    }
-
-    private fun calculateScaledDownSize(originalWidth: Int, originalHeight: Int, maxWidth: Int, maxHeight: Int): Pair<Int, Int> {
-        val widthRatio = originalWidth.toDouble() / maxWidth.toDouble()
-        val heightRatio = originalHeight.toDouble() / maxHeight.toDouble()
-        val scaleRatio = maxOf(widthRatio, heightRatio)
-
-        val scaledWidth = (originalWidth.toDouble() / scaleRatio).toInt()
-        val scaledHeight = (originalHeight.toDouble() / scaleRatio).toInt()
-
-        return Pair(scaledWidth, scaledHeight)
-    }
-
 
     @OptIn(ExperimentalSerializationApi::class)
     fun save() {
